ea9d4ef4e725921d7619577a387cda500f292c13
dejv0ld <david_johnson_1992@hotmail.com>
2023-11-13 10:35:26 +0100

    grejer

---
+++ src/store/api/postsApi.ts
@@ -0,0 +1,56 @@
+import { createApi } from '@reduxjs/toolkit/query/react';
+import { db } from '../../../firebase-config';
+import { addDoc, collection, getDocs, deleteDoc, doc, updateDoc, setDoc } from 'firebase/firestore';
+
+const firebaseBaseQuery = async ({ baseUrl, url, method, body }) => {
+  switch (method) {
+    case 'GET':
+      const snapshot = await getDocs(collection(db, url));
+      const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      return { data };
+    /*
+        case 'POST':
+          const docRef = await addDoc(collection(db, url), body);
+          return { data: { id: docRef.id, ...body } };
+
+        case 'DELETE':
+          const deleteRef = doc(db, url);
+          await deleteDoc(deleteRef);
+          return { data: 'Deleted successfully' };
+
+        case 'PUT':
+          const putRef = doc(db, url);
+          await setDoc(putRef, body, { merge: true });
+          return { data: 'Updated successfully' }; */
+
+    default:
+      throw new Error(`Unhandled method ${method}`);
+  }
+
+};
+
+export const postsApi = createApi({
+  reducerPath: 'postsApi',
+  baseQuery: firebaseBaseQuery,
+  tagTypes: ['posts'],
+  endpoints: (builder) => ({
+    createUser: builder.mutation({
+      query: ({ post }) => ({
+        baseUrl: '',
+        url: 'posts',
+        method: 'POST',
+        body: post
+      }),
+      invalidatesTags: ['posts'],
+    }),
+    getUsers: builder.query({
+      query: () => ({
+        baseUrl: '',
+        url: 'posts',
+        method: 'GET',
+        body: ''
+      }),
+      providesTags: ['posts'],
+    }),
+  })
+})
